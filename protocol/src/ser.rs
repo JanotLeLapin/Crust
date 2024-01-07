efd93c4cf0ff56f301c61e581e66f6efc4aa7f1f
Joseph DALY <joseph300905@gmail.com>
2024-01-07 19:07:42 +0100

    basic unit testing

--- protocol/src/ser.rs
+++ protocol/src/ser.rs
@@ -1,5 +1,6 @@
 macro_rules! varnum {
     ($name: ident, $type: ty, $unsigned_type: ty, $bytes: literal) => {
+        #[derive(Debug, PartialEq)]
         pub struct $name(pub $type);
 
         impl Serialize for $name {
@@ -44,5 +45,29 @@ impl<'a> Serialize for &'a str {
         res.append(&mut VarInt(size as i32).serialize());
         res.append(&mut self.chars().map(|c| c as u8).collect());
         res
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ser::Serialize;
+
+    #[test]
+    fn varint() {
+        assert_eq!(super::VarInt(0).serialize(), [0x00]);
+        assert_eq!(super::VarInt(1).serialize(), [0x01]);
+        assert_eq!(super::VarInt(2).serialize(), [0x02]);
+        assert_eq!(super::VarInt(127).serialize(), [0x7F]);
+        assert_eq!(super::VarInt(128).serialize(), [0x80, 0x01]);
+        assert_eq!(super::VarInt(255).serialize(), [0xFF, 0x01]);
+        assert_eq!(super::VarInt(2097151).serialize(), [0xFF, 0xFF, 0x7F]);
+        assert_eq!(super::VarInt(2147483647).serialize(), [0xFF, 0xFF, 0xFF, 0xFF, 0x07]);
+        assert_eq!(super::VarInt(-1).serialize(), [0xFF, 0xFF, 0xFF, 0xFF, 0x0F]);
+    }
+
+    #[test]
+    fn string() {
+        assert_eq!("foo".serialize(), [3, 0x66, 0x6F, 0x6F]);
+        assert_eq!("bar".serialize(), [3, 0x62, 0x61, 0x72]);
     }
 }
